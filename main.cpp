9f1735193c40bcff4a91c19c5779050e4d0fdff9
Ricardo Araujo <ricarrrdoluigi@gmail.com>
2021-08-18 02:26:27 -0300

    first

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <glm/glm.hpp>
+#include <glm/gtx/string_cast.hpp>
+#include <stb_image.h>
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+
+int main()
+{
+	glm::vec3 V{ 1, 2, 3 };
+
+	glfwInit();
+
+	glewInit();
+
+	std::cout << glm::to_string(V) << std::endl;
+	return 0;
+}
