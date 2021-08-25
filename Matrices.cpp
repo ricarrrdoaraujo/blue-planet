fe2586403059b4df232ac1f242492321e63f9d29
Ricardo Araujo <ricarrrdoluigi@gmail.com>
2021-08-25 00:44:03 -0300

    Scale Matrix

--- Matrices.cpp
+++ Matrices.cpp
@@ -27,8 +27,6 @@ void TranslationMatrix()
 	std::cout << "Translation Matrix" << std::endl;
 	std::cout << "=================" << std::endl;
 
-	
-
 	glm::mat4 I = glm::identity<glm::mat4>();
 	glm::vec3 T{ 10, 10, 10 };
 	glm::mat4 Translation = glm::translate(I, T);
@@ -47,9 +45,33 @@ void TranslationMatrix()
 	//PrintMatrix(Translation);
 }
 
+void ScaleMatrix()
+{
+	std::cout << std::endl;
+	std::cout << "=================" << std::endl;
+	std::cout << "Scale Matrix" << std::endl;
+	std::cout << "=================" << std::endl;
+
+	glm::mat4 I = glm::identity<glm::mat4>();
+	glm::vec3 ScaleAmount{ 2, 2, 2 };
+	glm::mat4 Scale = glm::scale(I, ScaleAmount);
+
+	PrintMatrix(Scale);
+
+	glm::vec4 Position{ 100, 100, 0, 1 };
+	glm::vec4 Direction{ 100, 100, 0, 0 };
+
+	Position = Scale * Position;
+	Direction = Scale * Direction;
+	std::cout << glm::to_string(Position) << std::endl;
+	std::cout << glm::to_string(Direction) << std::endl;
+
+	std::cout << std::endl;
+}
+
 int main()
 {
 	TranslationMatrix();
-
+	ScaleMatrix();
 	return 0;
 }
